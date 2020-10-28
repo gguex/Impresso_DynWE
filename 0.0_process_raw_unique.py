8b7b1c3600532ce39668501810cdda43183a3d27
gguex <guillaume.guex@gmail.com>
2020-10-28 16:04:59 +0100

    Processing of raw : added "-" treatments, option to remove ads

--- 0.0_process_raw_unique.py
+++ 0.0_process_raw_unique.py
@@ -6,7 +6,8 @@ from tqdm import tqdm
 # -- Input and output file path names
 
 data_to_test_path = "/home/gguex/Documents/data/impresso/JDG/JDG-1997.jsonl.bz2"
-output_file_path = "/home/gguex/Documents/data/impresso/JDG_text_only_new/JDG-1997.txt"
+#output_file_path = "/home/gguex/Documents/data/impresso/JDG_text_only_new/JDG-1997.txt"
+output_file_path = "/home/gguex/Documents/data/impresso/JDG_text_only_new/JDG-1997_ar.txt"
 
 # -- Constructing tokenizer
 
@@ -17,7 +18,9 @@ sent_tokenizer = nltk.data.load('tokenizers/punkt/french.pickle')
 # Opening json
 df = pd.read_json(data_to_test_path, lines=True)
 # Keeping ads and articles, text only
-processed_df = df[df["tp"].isin(["ad", "ar"])]["ft"].dropna()
+#processed_df = df[df["tp"].isin(["ad", "ar"])]["ft"].dropna()
+# Keeping only articles text
+processed_df = df[df["tp"].isin(["ar"])]["ft"].dropna()
 
 # Opening output file
 with open(output_file_path, "w") as output_file:
@@ -27,6 +30,11 @@ with open(output_file_path, "w") as output_file:
         for sentence in sent_list:
             # Keeping only alphas and "-"
             sentence_treated = re.sub(r'[^a-zA-ZÀ-ÿ\- ]', ' ', sentence)
+            # Removing "-" which aren't between two words
+            sentence_treated = re.sub(r'\- ', ' ', sentence_treated)
+            sentence_treated = re.sub(r' \-', ' ', sentence_treated)
+            sentence_treated = re.sub(r'\-$', ' ', sentence_treated)
+            sentence_treated = re.sub(r'^\-', ' ', sentence_treated)
             # Removing extra spaces and setting to lower
             sentence_treated = ' '.join(sentence_treated.lower().split())
             # If non-empty, write it
