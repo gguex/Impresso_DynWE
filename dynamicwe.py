39461eb9f9d6cfc03d78982a0431ecbe00c3b3c2
gguex <guillaume.guex@gmail.com>
2021-06-08 09:45:34 +0200

    Updated the w2v_aligned function, ref year not needed anymore (old version still there)

--- dynamicwe.py
+++ dynamicwe.py
@@ -20,9 +20,9 @@ class DynamicWordEmbedding:
         self.freq_list = []
         self.metadata = {}
 
-    def build_with_aligned_w2v(self, corpora_folder, corpus_list=None, embedding_name_list=None, ref_name=None,
-                               min_count=100, window=10, size=300, sample=1e-5, negative=10, alpha=0.025,
-                               ns_exponent=0.75, workers=6, sg=1):
+    def build_with_aligned_w2v_ref(self, corpora_folder, corpus_list=None, embedding_name_list=None, ref_name=None,
+                                   min_count=100, window=10, size=300, sample=1e-5, negative=10, alpha=0.025,
+                                   ns_exponent=0.75, workers=6, sg=1):
 
         # Folder and corpus list error management
         if not os.path.isdir(corpora_folder):
@@ -116,10 +116,103 @@ class DynamicWordEmbedding:
                 self.common_vocab = list(set(self.common_vocab) & set(corpus_vocab))
 
             # Create metadata
-            self.metadata = {"type": "aligned_w2v", "date": str(datetime.now()), "input_folder": corpora_folder,
+            self.metadata = {"type": "aligned_w2v_ref", "date": str(datetime.now()), "input_folder": corpora_folder,
                              "corpus_list": corpus_list, "ref_name": self.ref_name, "min_count": min_count,
                              "window": window, "size": size, "sample": sample, "negative": negative, "alpha": alpha,
                              "ns_exponent": ns_exponent, "sg": sg}
+
+    def build_with_aligned_w2v(self, corpora_folder, corpus_list=None, embedding_name_list=None,
+                               min_count=100, window=10, size=300, sample=1e-5, negative=10, alpha=0.025,
+                               ns_exponent=0.75, workers=6, sg=1):
+
+        # Folder and corpus list error management
+        if not os.path.isdir(corpora_folder):
+            raise FileNotFoundError(f"No such directory: '{corpora_folder}'")
+        file_list = os.listdir(corpora_folder)
+        file_list.sort()
+        if len(file_list) == 0:
+            raise FileNotFoundError(f"'{corpora_folder}' is empty")
+        if corpus_list is None:
+            corpus_list = file_list
+        else:
+            if corpus_list.__class__.__name__ != "list":
+                corpus_list = [corpus_list]
+            missing_corpus_list = [corpus for corpus in corpus_list if corpus not in file_list]
+            if len(missing_corpus_list) > 0:
+                raise FileNotFoundError(f"{missing_corpus_list} corpora are missing in {corpora_folder}")
+
+        # Embedding name list error management
+        if embedding_name_list is None:
+            embedding_name_list = corpus_list
+        else:
+            if embedding_name_list.__class__.__name__ != "list":
+                embedding_name_list = [embedding_name_list]
+            if len(embedding_name_list) != len(corpus_list):
+                raise ValueError("'embedding_name_list' must be same size as 'corpus_list'")
+        self.embedding_name_list = embedding_name_list
+
+        previous_model = None
+        # Make all embeddings
+        for i, corpus in tqdm(enumerate(corpus_list)):
+            if i == 0:
+                previous_model = Word2Vec(corpus_file=f"{corpora_folder}/{corpus}",
+                                          min_count=min_count,
+                                          window=window,
+                                          size=size,
+                                          sample=sample,
+                                          negative=negative,
+                                          alpha=alpha,
+                                          ns_exponent=ns_exponent,
+                                          workers=workers,
+                                          sg=sg)
+                self.common_vocab = list(previous_model.wv.vocab.keys())
+            else:
+                current_model = Word2Vec(corpus_file=f"{corpora_folder}/{corpus}",
+                                         min_count=min_count,
+                                         window=window,
+                                         size=size,
+                                         sample=sample,
+                                         negative=negative,
+                                         alpha=alpha,
+                                         ns_exponent=ns_exponent,
+                                         workers=workers,
+                                         sg=sg)
+
+                # Get year vocab and vectors
+                corpus_vocab = list(previous_model.wv.vocab.keys())
+                current_vocab = list(current_model.wv.vocab.keys())
+                corpus_vectors = previous_model.wv.__getitem__(corpus_vocab)
+                corpus_freq = [previous_model.wv.vocab.get(word).count for word in corpus_vocab]
+
+                # Align the year on ref
+                corpus_common_vocab = list(set(current_vocab) & set(corpus_vocab))
+                restrained_current_vectors = current_model.wv.__getitem__(corpus_common_vocab)
+                restrained_previous_vectors = previous_model.wv.__getitem__(corpus_common_vocab)
+                mat, _ = orthogonal_procrustes(restrained_previous_vectors, restrained_current_vectors)
+
+                # Save elements
+                self.embedding_list.append(corpus_vectors.dot(mat))
+                self.vocab_list.append(corpus_vocab)
+                self.freq_list.append(corpus_freq)
+                self.common_vocab = list(set(self.common_vocab) & set(corpus_vocab))
+
+                # Update previous model
+                previous_model = current_model
+
+        # Last update
+        corpus_vocab = list(previous_model.wv.vocab.keys())
+        corpus_vectors = previous_model.wv.__getitem__(corpus_vocab)
+        corpus_freq = [previous_model.wv.vocab.get(word).count for word in corpus_vocab]
+        self.embedding_list.append(corpus_vectors)
+        self.vocab_list.append(corpus_vocab)
+        self.freq_list.append(corpus_freq)
+        self.common_vocab = list(set(self.common_vocab) & set(corpus_vocab))
+
+        # Create metadata
+        self.metadata = {"type": "aligned_w2v", "date": str(datetime.now()), "input_folder": corpora_folder,
+                         "corpus_list": corpus_list, "ref_name": self.ref_name, "min_count": min_count,
+                         "window": window, "size": size, "sample": sample, "negative": negative, "alpha": alpha,
+                         "ns_exponent": ns_exponent, "sg": sg}
 
     def save(self, output_folder, overwrite=False):
 
